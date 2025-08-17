776a67ba8ae5e17dc6664c5055248d442772836a
Jatin757-343 <kumar.jatin2192005@gmail.com>
2025-08-17 12:58:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "22" => 22
+// "22avc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Jatin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Jatin" => true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);

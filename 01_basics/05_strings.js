b9b43556aa63831b3c3084356cb2bce46a33b382
Jatin757-343 <kumar.jatin2192005@gmail.com>
2025-08-19 06:42:32 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const newString = "Jatin"
+console.log(newString);
+
+
+const name = "Jatin"
+age = 20
+
+console.log(`Hello my name is ${name} and i am ${age} yrs old`);
+
+const myString = "   Jatin    "
+console.log(myString.trim()); //Trims unwanted space
+
+const url = "https://youtube.com/shorts/HWe5ZnB2BEo?si=1qMjgJPQh0rmO6mW"
+
+console.log(url.replace('shorts', '---')); // Can replace any specific content 
+
+console.log(url.includes('youtube'));

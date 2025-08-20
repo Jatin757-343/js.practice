7ae0ed93874d9ab9c098c4294b6dcda7e5becded
Jatin757-343 <kumar.jatin2192005@gmail.com>
2025-08-20 07:03:56 +0000

    Objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+ // Singleton
+// create.objects   // constructir method
+
+ // Object literals
+
+const mySym = Symbol("key1")
+
+ const JsUser = {
+    Name: "Jatin",
+    "full Name": "Jatin kumar",
+    [mySym]: "mykey1",
+    Age: 20,
+    Location: "Chamba",
+    email: "Jatin@gmail.com",
+    isLoggedIn: false,
+    LastLoginDays: ["Monday", "Tuesday"]
+ }
+
+//  console.log(JsUser.email)
+//  console.log(JsUser["email"]);
+//  console.log(mySym);
+ 
+ JsUser.email = "Jatiin123@mail.com"
+//  Object.freeze(JsUser)
+ JsUser.email = "Jatiin123@microsoft.com"
+//  console.log(JsUser);
+ 
+
+ JsUser.greeting = function(){
+    console.log("Hello js user");  
+ }
+ 
+ JsUser.greetingTwo = function(){
+     console.log(`Hello ${this.Name}`);
+ }
+
+
+ console.log(JsUser.greeting());
+ console.log(JsUser.greetingTwo());
+ 

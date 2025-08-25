a9905d692f5aad8fbf33b72be662aff61d98a290
Jatin757-343 <kumar.jatin2192005@gmail.com>
2025-08-25 11:28:00 +0000

    functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+function myName() {
+console.log("J");
+console.log("A");
+console.log("T");
+console.log("I");
+console.log("N");
+}
+
+// myName()
+
+// function addTwoNumbers(num1, num2) {
+//    console.log(num1 + num2);
+// }
+
+// const result = addTwoNumbers(5,6)
+// console.log('Result: ', result);
+
+function addTwoNumbers(num1, num2) {
+//    let result = (num1 + num2)
+//    return result
+    return num1 + num2
+}
+
+const result = addTwoNumbers(5,6)
+// console.log('Result: ', result);
+
+
+function userlogInMessage(username = "Bot"){
+    if(!username){
+        console.log("Please enter your username")
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(userlogInMessage("Jatin"))
+// console.log(userlogInMessage("Jatin"))
